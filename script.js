c51c0ac36b0c3d637adb7038e2ee2fc8ebcdbc7b
Brian Denmark <testingarea479@gmail.com>
2022-09-29 16:41:40 -0700

    init commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const pokémon = Object.freeze([
+    { "id": 1,   "name": "Bulbasaur",  "types": ["poison", "grass"] },
+    { "id": 5,   "name": "Charmeleon", "types": ["fire"] },
+    { "id": 9,   "name": "Blastoise",  "types": ["water"] },
+    { "id": 12,  "name": "Butterfree", "types": ["bug", "flying"] },
+    { "id": 16,  "name": "Pidgey",     "types": ["normal", "flying"] },
+    { "id": 23,  "name": "Ekans",      "types": ["poison"] },
+    { "id": 24,  "name": "Arbok",      "types": ["poison"] },
+    { "id": 25,  "name": "Pikachu",    "types": ["electric"] },
+    { "id": 37,  "name": "Vulpix",     "types": ["fire"] },
+    { "id": 52,  "name": "Meowth",     "types": ["normal"] },
+    { "id": 63,  "name": "Abra",       "types": ["psychic"] },
+    { "id": 67,  "name": "Machamp",    "types": ["fighting"] },
+    { "id": 72,  "name": "Tentacool",  "types": ["water", "poison"] },
+    { "id": 74,  "name": "Geodude",    "types": ["rock", "ground"] },
+    { "id": 87,  "name": "Dewgong",    "types": ["water", "ice"] },
+    { "id": 98,  "name": "Krabby",     "types": ["water"] },
+    { "id": 115, "name": "Kangaskhan", "types": ["normal"] },
+    { "id": 122, "name": "Mr. Mime",   "types": ["psychic"] },
+    { "id": 133, "name": "Eevee",      "types": ["normal"] },
+    { "id": 144, "name": "Articuno",   "types": ["ice", "flying"] },
+    { "id": 145, "name": "Zapdos",     "types": ["electric", "flying"] },
+    { "id": 146, "name": "Moltres",    "types": ["fire", "flying"] },
+    { "id": 148, "name": "Dragonair",  "types": ["dragon"] }
+]);
+
+// use filter to only return pokémon names starting with "B"
+console.log("\npokémon name begins with 'B'")
+const bListPkmn = pokémon.filter( p => p.name[0] ==="B")
+bListPkmn.forEach( p => console.log(p.name))
+
+// return only the pokémon ids which are divisible by 3
+console.log("\npokémon id divisible by 3")
+const byThree = pokémon.filter( p => p.id % 3 === 0).map( p => p)
+byThree.forEach( p => console.log(p.name))
+
+// return only pokémon types which are fire
+console.log("\npokémon type 'fire'")
+const typeFire = pokémon.filter( p => p.types.includes("fire")).map( p => p)
+typeFire.forEach(p => console.log(p.name))
